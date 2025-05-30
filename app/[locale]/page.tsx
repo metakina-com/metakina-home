819dd1449807fbe15bc178258898485d3ab887c9
dingyiwei <1185798283@qq.com>
2025-05-30 11:07:37 +0800

    测试自动化部署

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,10 @@
-import ChallengePage from "./content/challenge";
-import ConsultPage from "./content/consult";
-import ContactUsPage from "./content/contactUs";
-import CooperationPlatformPage from "./content/cooperationPlatform";
-import CoreTeamPage from "./content/coreTeam";
-import NumberPage from "./content/number";
-import SolutionPage from "./content/solution";
+import ChallengePage from "./content/challenge"
+import ConsultPage from "./content/consult"
+import ContactUsPage from "./content/contactUs"
+import CooperationPlatformPage from "./content/cooperationPlatform"
+import CoreTeamPage from "./content/coreTeam"
+import NumberPage from "./content/number"
+import SolutionPage from "./content/solution"
 
 export default function Home() {
   return (
@@ -17,5 +17,6 @@ export default function Home() {
       <NumberPage />
       <ContactUsPage />
     </>
-  );
+  )
 }
+
