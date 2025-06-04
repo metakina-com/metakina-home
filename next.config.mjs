786108195ec4b0faf83839d7cce424bbc17e12f7
dingyiwei <1185798283@qq.com>
2025-06-04 17:19:26 +0800

    添加  output: "standalone"

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,15 @@ const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       ...(process.env.R2_PUBLIC_URL
         ? [
-          {
-            hostname: process.env.R2_PUBLIC_URL.replace("https://", ""),
-          },
-        ]
+            {
+              hostname: process.env.R2_PUBLIC_URL.replace("https://", ""),
+            },
+          ]
         : []),
     ],
   },
@@ -19,8 +20,8 @@ const nextConfig = {
     removeConsole:
       process.env.NODE_ENV === "production"
         ? {
-          exclude: ["error"],
-        }
+            exclude: ["error"],
+          }
         : false,
   },
 }
