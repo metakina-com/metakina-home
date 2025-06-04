b745ba4022d06ece1b17aab20c1edf8da15c0c32
dingyiwei <1185798283@qq.com>
2025-06-04 13:14:03 +0800

    export const dynamic = 'force-static' 删除

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,6 @@ import { siteConfig } from '@/config/site'
 import type { MetadataRoute } from 'next'
 
 const siteUrl = siteConfig.url
-export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
   return {
