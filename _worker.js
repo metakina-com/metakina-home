b2606befa82f6747bb5600830dc8bccdf9239487
dingyiwei <1185798283@qq.com>
2025-06-07 11:51:59 +0800

    添加反向代理

---
+++ _worker.js
@@ -0,0 +1,15 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith('/')) {
+      url.hostname = 'www.rwaform.dappweb.cn';
+
+      const new_request = new Request(url, request);
+
+      return fetch(new_request);
+    }
+
+    return env.ASSETS.fetch(request);
+  },
+};
