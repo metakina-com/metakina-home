792df288849c37144701e8aafa4d7f324647f770
dingyiwei <1185798283@qq.com>
2025-06-07 17:42:46 +0800

    +1

---
+++ functions/api/[[path]].js
@@ -0,0 +1,11 @@
+export async function onRequest(context) {
+  const { request } = context;
+  const url = new URL(request.url);
+  const apiUrl = `https://rwaform.dappweb.cn${url.pathname}`;
+
+  return fetch(apiUrl, {
+    headers: request.headers,
+    method: request.method,
+    body: request.body,
+  });
+}
